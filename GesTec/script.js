927050227129baee841cc44067adbeb1e1d2de32
João Paulo <joaopaulodev01@gmail.com>
2025-02-27 12:54:26 -0300

    Add files via upload

--- GesTec/script.js
+++ GesTec/script.js
@@ -13,3 +13,33 @@ function nextImage(){
     document.getElementById("radio" + count).checked = true;
 
 }
+
+// Selecione o botão de troca de tema
+const toggleThemeButton = document.getElementById('color_mode');
+
+// Adicione um evento de clique ao botão
+toggleThemeButton.addEventListener('click', () => {
+  // Verifique se o tema atual é claro ou escuro
+  const currentTheme = document.body.classList.contains('theme-light') ? 'light' : 'dark';
+
+  // Troque o tema
+  if (currentTheme === 'light') {
+    document.body.classList.remove('theme-light');
+    document.body.classList.add('theme-dark');
+    document.querySelector('.container').classList.remove('theme-light');
+    document.querySelector('.container').classList.add('theme-dark');
+    document.querySelectorAll('.form-content label').forEach((label) => {
+      label.classList.remove('theme-light');
+      label.classList.add('theme-dark');
+    });
+  } else {
+    document.body.classList.remove('theme-dark');
+    document.body.classList.add('theme-light');
+    document.querySelector('.container').classList.remove('theme-dark');
+    document.querySelector('.container').classList.add('theme-light');
+    document.querySelectorAll('.form-content label').forEach((label) => {
+      label.classList.remove('theme-dark');
+      label.classList.add('theme-light');
+    });
+  }
+});
